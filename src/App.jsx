db9c42d6dba5dde0abcc5456f925ed5b6c3beabd
Kalmyrza <zholchubekovk@gmail.com>
2023-11-04 00:18:09 +0600

    Add: text area use state and Alatoo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,56 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import AlaToo from './assets/images/Alatoo.png';
+import './App.css';
 
-export default function NewPost() {
-  
-  
-  function getRandomColor() {
+function TextArea() {
+  const [text, setText] = useState('');
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [boxShadowColor, setBoxShadowColor] = useState('#333');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+    const randomTop = Math.random() * 440;
+    const randomLeft = Math.random() * 440; 
+    setPosition({ top: randomTop, left: randomLeft });
+
+    const randomColor = getRandomColor();
+    setBorderColor(randomColor);
+  };
+
+  const textAreaStyles = {
+    position: 'absolute',
+    top: `${position.top}px`,
+    left: `${position.left}px`,
+    borderColor: borderColor
+  };
+
+  const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-  
-  const [postContent, setPostContent] = useState('HI'); // Declare a state variable...
-  const [backgroundColor, setBackgroundColor] = useState('white'); // Initial background color
-  
+  };
+
+
   return (
-    <>
-    <label>
-      Enter some markdown:
-      <textarea
-        value={postContent}
-        onChange={e => setPostContent(e.target.value)}
-      />
-    </label>
-    <hr />
-  </>
+    <div className="myApp">
+      <img src={AlaToo} alt="Ala too logo"  className='rotate'/>
+      <h1>Impossible TextArea</h1>
+      <div className="textarea-container">
+        <textarea
+          rows="8"
+          cols="25"
+          placeholder="Type something..."
+          value={text}
+          onChange={handleChange}
+          className="textarea"
+          style={textAreaStyles}
+        />
+      </div>
+    </div>
   );
-  
 }
 
+export default TextArea;
